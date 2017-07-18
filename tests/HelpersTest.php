7aa76c05eb85236b9ca8fa9c74ff67281c014b17
puzhengchao <zhengchaopu@gmail.com>
2017-07-18 20:59:43 +0800

    Update tests

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -15,10 +15,12 @@ class HelpersTest extends TestCase
 {
     public function testEmojiUtf8Bytes()
     {
+    	$bytesOne = emoji_utf8_bytes(0x0023);
         $bytesTwo = emoji_utf8_bytes(0x00AE);
         $bytesThree = emoji_utf8_bytes(0x3299);
         $bytesFour = emoji_utf8_bytes(0x26F9).emoji_utf8_bytes(0xFE0F).emoji_utf8_bytes(0x200D).emoji_utf8_bytes(0x2642).emoji_utf8_bytes(0xFE0F);
 
+        $this->assertSame('#', $bytesOne);
         $this->assertSame('®', $bytesTwo);
         $this->assertSame('㊙', $bytesThree);
         $this->assertSame('⛹️‍♂️', $bytesFour);
